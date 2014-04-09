351a6719ec84528693519e44f43fa80015fb2cad
Mike Czepiel <mike@czepiel.net>
2014-04-09 22:30:25 +0000

    Add component task-cell

---
+++ ui/task-cell.reel/task-cell.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/task-cell.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TaskCell
+ * @extends Component
+ */
+exports.TaskCell = Component.specialize(/** @lends TaskCell# */ {
+    constructor: {
+        value: function TaskCell() {
+            this.super();
+        }
+    }
+});
